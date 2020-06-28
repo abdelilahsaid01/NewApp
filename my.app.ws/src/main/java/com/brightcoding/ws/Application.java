469d499d15f191ac87cd6b20d917ba1fccc8368c
abdelilahsaid01 <abdelilahsaid01@gmail.coml>
2020-06-28 15:32:14 +0100

    "hello world"

--- my.app.ws/src/main/java/com/brightcoding/ws/Application.java
+++ my.app.ws/src/main/java/com/brightcoding/ws/Application.java
@@ -13,6 +13,7 @@ public class Application extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		System.out.println("SpringBoot Start.........");
+		System.out.println("hello word");
 	}
 	
 	 @Override
